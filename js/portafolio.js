b74648324afd2131e1e2fa24d5b9dbace1d74e56
luism854 <luis854.fer@gmail.com>
2023-08-27 19:54:54 -0500

    fin

--- js/portafolio.js
+++ js/portafolio.js
@@ -9,4 +9,46 @@ function show() {
     box.style.display = "none";
     box.removeEventListener("click", hidden);
   }
-  
+  const scrollButton = document.getElementById('scrollButton');
+  const section1 = document.querySelector('.section-1')
+  const section2 = document.querySelector('.hidden-section-2');
+
+  scrollButton.addEventListener('click', () => {
+  section1.style.transform = 'translateY(-100%)'; // Cambia la propiedad transform
+  if (section2.classList.contains("hidden-section-2")) {
+    section2.classList.remove("hidden-section-2");
+    section2.classList.add("section-2");
+  }
+});
+
+// const scrollButton2 = document.getElementById('scrollButton2');
+// const section22 = document.querySelector('.section-2')
+// const section11 = document.querySelector('.hidden-section-1');
+
+
+//   scrollButton2.addEventListener('click', () => {
+//     section22.style.transform = 'translateY(0)';
+//     if (section2.classList.contains(".section-2")){
+//       section11.classList.remove(".section-2");
+//       section11.classList.add(".hidden-section-1");
+//     }
+// });
+
+const scrollLeftButton = document.querySelector(".scroll-left");
+const scrollRightButton = document.querySelector(".scroll-right");
+const projets = document.querySelector(".projets");
+
+scrollLeftButton.addEventListener("click", () => {
+  projets.scrollBy({
+    left: -200,
+    behavior: "smooth",
+  });
+});
+
+scrollRightButton.addEventListener("click", () => {
+  projets.scrollBy({
+    left: 200,
+    behavior: "smooth",
+  });
+});
+
