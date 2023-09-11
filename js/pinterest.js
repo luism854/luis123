5940fe639b9837b2db2d2c69303773f13551cd6f
ersfsafsa <info@gmail.com>
2023-09-11 11:29:37 -0500

    act

--- js/pinterest.js
+++ js/pinterest.js
@@ -16,3 +16,14 @@ window.addEventListener('scroll', function() {
     
     lastScrollTop = scrollTop;
 });
+
+window.addEventListener('scroll', function() {
+    const fixedBox = document.getElementById('fixedBox');
+    const scrollY = window.scrollY;
+
+    if (scrollY > 0) {
+        fixedBox.style.boxShadow = '0px -10px 15px rgba(0, 0, 0, 0.5)';
+    } else {
+        fixedBox.style.boxShadow = 'none';
+    }
+});
