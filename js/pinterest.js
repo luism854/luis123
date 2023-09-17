519641a27d02b3b9ac2aae1d6360996f31d71cf2
luism854 <luis854.fer@gmail.com>
2023-09-17 16:00:05 -0500

    finalizar-

--- js/pinterest.js
+++ js/pinterest.js
@@ -1,3 +1,4 @@
+//scroll para el cel. para que no se esconda la barra
 var barra = document.querySelector('.barra-cel');
 var lastScrollTop = 0;
 
@@ -17,6 +18,7 @@ window.addEventListener('scroll', function() {
     lastScrollTop = scrollTop;
 });
 
+//sirve para agregar un box-shadow en la pagina en pc, al bajar aparece
 window.addEventListener('scroll', function() {
     const fixedBox = document.getElementById('fixedBox');
     const scrollY = window.scrollY;
@@ -26,4 +28,15 @@ window.addEventListener('scroll', function() {
     } else {
         fixedBox.style.boxShadow = 'none';
     }
+});
+
+//redireccionador a la pagina de la imagen
+document.addEventListener("DOMContentLoaded", function () {
+    // Obtén el elemento del div con la imagen por su id
+    var imagenDiv = document.getElementById("imagenDiv");
+    // Agrega un evento de clic al div para redirigir la página
+    imagenDiv.addEventListener("click", function () {
+        // Redirige a la URL deseada
+        window.location.href = "pinterest img/pinterest1.html";
+    });
 });
