af5265be0cb165babcf4ab6cd24b0f2318649785
luism854 <luis854.fer@gmail.com>
2023-09-30 21:34:03 -0500

    fin-carrito

--- js/carrito.js
+++ js/carrito.js
@@ -1,20 +1,43 @@
 let numRestar = document.getElementById("numRestar");
 let numSumar = document.getElementById("numSumar");
 let numView = document.getElementById("numView");
+let numView2 = document.getElementById("cantProductos")
+let subT = 0
 
 numRestar.addEventListener("click", function(){
     let aux = numView.innerHTML.toString();
-    let auxInt = parseInt(aux) - 1;
-    if(aux>0){
+    let auxInt = parseInt(aux) -1;
+    if(auxInt>=0){
         console.log(auxInt)
         document.getElementById("numView").innerText = auxInt.toString()
+        document.getElementById("cantProductos").innerText = auxInt.toString()
+        if (auxInt == 1) {
+            document.getElementById("Product").textContent = " Producto";
+        }else{
+            document.getElementById("Product").textContent = " Productos";
+        }
+        subT -= 112.350;
+        document.getElementById("subTotal").textContent = subT.toFixed(3);
+        if(auxInt == 0){
+            document.getElementById("subTotal").textContent ="0"
+        }
     }
 });
 numSumar.addEventListener("click", function(){
     let aux = numView.innerHTML.toString();
-    let auxInt = parseInt(aux);
-    if(auxInt<10){
-        auxInt++;
+    let auxInt = parseInt(aux) +1;
+    if(auxInt<=10){
         document.getElementById("numView").innerText = auxInt.toString()
+        document.getElementById("cantProductos").innerHTML = auxInt.toString()
+        if (auxInt == 1) {
+            document.getElementById("Product").textContent = " Producto";
+        }else{
+            document.getElementById("Product").textContent = " Productos";
+        }
+        subT +=112.350;
+        document.getElementById("subTotal").textContent = subT.toFixed(3);
+        if(auxInt == 0){
+            document.getElementById("subTotal").textContent ="0"
+        }
     }
 });
