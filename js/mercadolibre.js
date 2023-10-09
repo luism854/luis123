1f1f27200d1bd65e964d9099142d63b61467defa
luism854 <luis854.fer@gmail.com>
2023-10-09 11:42:42 -0500

    a

--- js/mercadolibre.js
+++ js/mercadolibre.js
@@ -6,13 +6,26 @@ class Automovil{
         this.modelo = modelo;
     }
 }
-let auto1 = new Automovil("Toyota", "TXl");
-alert(auto1.marca+" "+auto1.modelo);
 
-inputBusqueda.addEventListener("keydown", function(){
-    let mainContent = document.getElementById("mainContent");
-    let boxProducto = document.createElement("div");
-    mainContent.appendChild(boxProducto);
-    boxProducto.setAttribute("class", "box-producto");
+inputBusqueda.addEventListener("keydown", function(event){
+    if(event.key == "Enter"){
+        let mainContent = document.getElementById("mainContent");
+        let boxProducto = document.createElement("div");
+        mainContent.appendChild(boxProducto);
+        boxProducto.setAttribute("class", "box-producto");
+
+        let boxImg = document.createElement("div");
+        boxProducto.appendChild(boxImg);
+        boxImg.setAttribute("class", "box-img");
+        let ImgAuto = document.createElement("img");
+        boxImg.appendChild(ImgAuto);
+        ImgAuto.setAttribute('src','imagenes/paisaje-770x900.jpg');
+        ImgAuto.setAttribute('class','img-auto')
+
+        let boxInfo = document.createElement('div');
+        boxProducto.appendChild(boxInfo);
+        boxInfo.setAttribute('class','box-info');
+    }
+
 });
 
