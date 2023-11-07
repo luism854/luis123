5121b20887e1cead41b5fae368f81b6c385d2f92
luism854 <luism854.fer@gmail.com>
2023-11-07 11:19:43 -0500

    fin

--- js/mercadolibre.js
+++ js/mercadolibre.js
@@ -17,8 +17,8 @@ let img1 = "imagenes/ford-scape.webp";
 let img2 = "imagenes/peugeot.webp";
 let img3 = "imagenes/peugeot2.webp";
 
-let auto1 = new Automovil("Ford", "Fiesta 1.6 Titanium Mecanico", 41900000, img, 2016, "81.000" , "Envigado - Atioquia " );
-let auto2 = new Automovil("Ford", "Escape 2.0 Titanium", 78000000, img1, 2016, "30.000" , "Engativa - Bogotá - D.C." );
+let auto1 = new Automovil("Ford Fiesta", "1.6 Titanium Mecanico", 41900000, img, 2016, "81.000" , "Envigado - Atioquia " );
+let auto2 = new Automovil("Ford Escape", "2.0 Titanium", 78000000, img1, 2016, "30.000" , "Engativa - Bogotá - D.C." );
 let auto3 = new Automovil("Peugeot", "Partner Cargo", 38500000, img2, 2019, "65.600" , "Suba - Bogotá D.C." );
 let auto4 = new Automovil("Peugeot", "206 1.4 Xr", 27500000, img3, 2019, "78.300" , "Fontibón - Bogotá D.C." );
 
@@ -31,7 +31,7 @@ inputBusqueda.addEventListener("input", function(event){
     mainContent.innerHTML = '';
 
     for(let auto of autos) {
-        if(auto.marca.toLowerCase() === userInput) {
+        if(userInput === '' || auto.marca.toLowerCase().includes(userInput)) {
             // if (event.key == "Enter"){ 
             //caja producto
             let boxProducto = document.createElement("div");
@@ -89,3 +89,60 @@ inputBusqueda.addEventListener("input", function(event){
         }
     }
 });
+
+for(let auto of autos) {
+        // if (event.key == "Enter"){ 
+        //caja producto
+        let boxProducto = document.createElement("div");
+        mainContent.appendChild(boxProducto);
+        boxProducto.setAttribute("class", "box-producto");
+        //caja imagen
+        let boxImg = document.createElement("div");
+        boxProducto.appendChild(boxImg);
+        boxImg.setAttribute("class", "box-img");
+
+        let imgAuto = document.createElement("img");
+        boxImg.appendChild(imgAuto);
+        imgAuto.setAttribute("src", auto.imagen);
+        imgAuto.setAttribute("class","img-auto");
+
+        //caja información
+        let boxInfo = document.createElement("div");
+        boxProducto.appendChild(boxInfo);
+        boxInfo.setAttribute("class", "box-info");
+
+        let marca = document.createElement("label");
+        boxInfo.appendChild(marca);
+        let txtNodeMarca = document.createTextNode(auto.marca + "  " + auto.modelo);
+        marca.appendChild(txtNodeMarca);
+        marca.setAttribute("class", "marca");
+
+        ///precio
+        let precio = document.createElement("label");
+        boxInfo.appendChild(precio);
+        //línea para convertir el número a formato de precio con . en los miles
+        let precioStr = Intl.NumberFormat("de-DE").format(auto.precio);
+        let txtNodePrecio = document.createTextNode("$ "+ precioStr);
+        precio.appendChild(txtNodePrecio);
+        precio.setAttribute("class", "precio");
+
+        //año 
+        let año = document.createElement("label");
+        boxInfo.appendChild(año);
+
+        let txtNodeAño = document.createTextNode(`${auto.año} • ${auto.km} km • ${auto.ciudad}`)
+        año.appendChild(txtNodeAño)
+        año.setAttribute("class", "año")
+
+        //let icon
+        let boxCorazon = document.createElement("div");
+        boxImg.appendChild(boxCorazon);
+        let icon = document.createElement("i");
+        boxCorazon.appendChild(icon);
+        boxCorazon.setAttribute("class", "box-corazon");
+        icon.setAttribute("class", "icon-corazon fa-regular fa-heart");
+
+        //let linea
+        let lineaDiv = document.createElement("hr");
+
+}
