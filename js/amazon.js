915077f5430c64ae381f915e5b85e95ac0a6b4aa
luism854 <luis854.fer@gmail.com>
2023-12-13 22:28:44 -0500

    fin

--- js/amazon.js
+++ js/amazon.js
@@ -1,21 +1,34 @@
-let calificacion1 = document.getElementById("calificacion1")
-let calificacion2 = document.getElementById("calificacion2")
-let calificacion3 = document.getElementById("calificacion3")
-let calificacion4 = document.getElementById("calificacion4")
-let calificacion5 = document.getElementById("calificacion5")
 let btngraficar = document.getElementById("btngraficar")
 
 btngraficar.addEventListener("click", function(){
-    let sumaCalificacion = (Number(calificacion1.value)+Number(calificacion2.value)+Number(calificacion3.value)+Number(calificacion4.value)+Number(calificacion5.value))
 
-    let promedio1Estrella = (Number(calificacion1.value)*100)/sumaCalificacion;
-    let promedio2Estrella = (Number(calificacion2.value)*100)/sumaCalificacion;
-    let promedio3Estrella = (Number(calificacion3.value)*100)/sumaCalificacion;
-    let promedio4Estrella = (Number(calificacion4.value)*100)/sumaCalificacion;
-    let promedio5Estrella = (Number(calificacion5.value)*100)/sumaCalificacion;
+    let calificacion1 = parseInt(document.getElementById("calificacion1").value) || 0;
+    let calificacion2 = parseInt(document.getElementById("calificacion2").value) || 0;
+    let calificacion3 = parseInt(document.getElementById("calificacion3").value) || 0;
+    let calificacion4 = parseInt(document.getElementById("calificacion4").value) || 0;
+    let calificacion5 = parseInt(document.getElementById("calificacion5").value) || 0;
 
-    
+    let total = calificacion1 + calificacion2 + calificacion3 + calificacion4 + calificacion5;
 
+    let promedio1Estrella = (calificacion1 / total) * 100;
+    let promedio2Estrella = (calificacion2 / total) * 100;
+    let promedio3Estrella = (calificacion3 / total) * 100;
+    let promedio4Estrella = (calificacion4 / total) * 100;
+    let promedio5Estrella = (calificacion5 / total) * 100;
 
-    alert(promedio1Estrella);
-})
+    mostrarPorcentaje("porcentaje1", "barra1", promedio1Estrella);
+    mostrarPorcentaje("porcentaje2", "barra2", promedio2Estrella);
+    mostrarPorcentaje("porcentaje3", "barra3", promedio3Estrella);
+    mostrarPorcentaje("porcentaje4", "barra4", promedio4Estrella);
+    mostrarPorcentaje("porcentaje5", "barra5", promedio5Estrella);
+
+});
+
+function mostrarPorcentaje(idPorcentaje, idBarra, porcentaje) {
+    let elementoPorcentaje = document.getElementById(idPorcentaje);
+    elementoPorcentaje.innerHTML = porcentaje.toFixed(1) + "%";
+
+    let barra = document.getElementById(idBarra);
+
+    barra.style.width = porcentaje + "%";
+}
